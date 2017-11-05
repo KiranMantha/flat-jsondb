545a860160ce71da7e92abba3b7da62ba9788106
KiranMantha <kiranv.mantha@gmail.com>
2017-11-05 17:16:23 +0530

    Refractoring

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var path = require('path');
 
-app.use(express.static(__dirname + '/public' ));
-app.use(express.static(__dirname + '/db'));
+app.use(express.static(__dirname + '/lib'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
