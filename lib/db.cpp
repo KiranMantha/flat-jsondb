ded8e9d5daee360d1ebb8c4c04458e0bd058e8d8
KiranMantha <kiranv.mantha@gmail.com>
2017-11-12 22:46:19 +0530

    basic program to read file contents

--- lib/db.cpp
+++ lib/db.cpp
@@ -1,7 +1,39 @@
+/*
+    1. run command g++ -o c++.exe db.cpp
+    2. run command c++
+*/
+
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+int parse(string file_name)
+{
+    int sum = 0;
+    int x;
+    //opens file
+    std::ifstream file(file_name.c_str()); 
+    if (!file)
+    {
+        cout << "Cannot open file\n";
+        exit(1);
+    }
+    cout << "File is opened\n";    
+    while (file >> x) {
+        sum = sum + x;
+    }
+    //closes file
+    std::cout << file.rdbuf();
+    file.close();
+    return sum;
+}
+
+int main()
+{
+    string st = "abc.txt";
+    int sum = parse(st);
+    //calls the parse function
+    cout << "Sum = " << sum << endl; 
     return 0;
 }
