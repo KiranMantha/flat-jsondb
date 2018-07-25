fdb1333ceb343a7242f485af84d8c9f0846c4911
KiranMantha <kiranv.mantha@gmail.com>
2018-07-25 11:38:09 +0530

    added update and remove logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const db = require('./jsondb')(path.resolve(__dirname , './data'));
-db.createTable(['movies', 'actors', 'years']);
-//db.createTable('movies');
+//db.createTable(['movies', 'actors', 'years']);
+console.log(db.createTable('movies'));
 const rec = db.insert('movies', { title: 'Mission Impossible' });
 console.log(db.getById('movies', rec.id));
+console.log(db.updateById('movies', rec.id, { favorite: true }));
--- jsondb.js
+++ jsondb.js
@@ -6,24 +6,30 @@ const _ = Object.assign({}, lodash, lodashid);
 function jsondb(dataBasePath) {
     let utils = new Utils(dataBasePath);
     let cache = {};
-    
+
     utils.mkdirSync();
 
+    _.createId = function() {
+        return Date.now();
+    }
+
     // Create table JSON
-    this.createTable = function(tableName) {
-        if(typeof tableName === 'string') {
-            let tableData = utils.createTable(tableName);
+    this.createTable = function (tableName) {
+        let tableData;
+        if (typeof tableName === 'string') {
+            tableData = utils.createTable(tableName);
             cache[tableName] = tableData;
-        } else if(tableName.slice){
-            _.each(tableName, function(table) {
-                let tableData = utils.createTable(table);
+        } else if (tableName.slice) {
+            _.each(tableName, function (table) {
+                tableData = utils.createTable(table);
                 cache[table] = tableData;
             });
         }
+        return tableData;
     }
 
     // CURD Functions
-    this.get = function(tableName) {
+    this.get = function (tableName) {
         try {
             if (utils.checkTable(tableName) && !cache[tableName]) {
                 const data = this.utils.readFile(tableName);
@@ -32,30 +38,39 @@ function jsondb(dataBasePath) {
             } else {
                 return cache[tableName];
             }
-        } catch (error) {
-        }
+        } catch (error) {}
     }
 
-    this.getById = function(tableName, id) {
-        return  _.getById(cache[tableName], id);
+    this.getById = function (tableName, id) {
+        return _.getById(cache[tableName], id);
     }
 
-    this.insert = function(tableName, data) {
+    this.insert = function (tableName, data) {
         const rec = _.insert(cache[tableName], data);
         utils.save(tableName, cache[tableName]);
         return rec;
     }
 
     this.updateById = function (tableName, id, data) {
-
+        const rec = _.updateById(cache[tableName], id, data);
+        utils.save(tableName, cache[tableName]);
+        return rec;
     }
 
-    this.removeById = function(tableName, id) {
+    this.updateWhere = function (tableName, whereAttrs, attrs) {
+        const recs = _.updateWhere(cache[tableName], whereAttrs, attrs);
+        utils.save(tableName, cache[tableName]);
+        return recs;
+    }
 
+    this.removeById = function (tableName, id) {
+        _.removeById(cache[tableName], id);
+        utils.save(tableName, cache[tableName]);
     }
 
     this.removeWhere = function (tableName, whereAttrs) {
-
+        _.removeWhere(cache[tableName], whereAttrs);
+        utils.save(tableName, cache[tableName]);
     }
 
     return this;
