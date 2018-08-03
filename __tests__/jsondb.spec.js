6113706e99c9c85f6f39e57891deee827460138e
Mantha Venkata Satya Sai Kiran <mansatya@publicisgroupe.net>
2018-08-03 15:23:24 +0530

    inprogress: moving to async/await approach and implemented prettier

--- __tests__/jsondb.spec.js
+++ __tests__/jsondb.spec.js
@@ -1,155 +1,159 @@
-const jsondb = require('../jsondb');
-const path = require('path');
-const fs = require('graceful-fs');
-const del = require('del');
-const lodash = require('lodash');
-const lodashid = require('lodash-id');
+const jsondb = require("../jsondb");
+const path = require("path");
+const fs = require("graceful-fs");
+const del = require("del");
+const lodash = require("lodash");
+const lodashid = require("lodash-id");
 const _ = Object.assign({}, lodash, lodashid);
 
-describe('jsondb', () => {
-    let db = null;
-    let tempdir = path.normalize(__dirname + '/temp-db');
-    beforeAll(() => {
-        fs.mkdirSync(tempdir);
-        db = jsondb(tempdir);
+describe("jsondb", () => {
+  let db = null;
+  let tempdir = path.normalize(__dirname + "/temp-db");
+  beforeAll(() => {
+    fs.mkdirSync(tempdir);
+    db = jsondb(tempdir);
+  });
+
+  beforeEach(async () => {
+    db.cleanCache();
+    await db.createTable("movies");
+  });
+
+  afterEach(() => {
+    db.truncateTable("movies");
+  });
+
+  afterAll(() => {
+    del.sync(tempdir + "/**", {
+      force: true
     });
+    db = null;
+  });
 
-    beforeEach(() => {
-        db.cleanCache();
-        db.createTable('movies');
+  test("create movies.json", () => {
+    expect(fs.existsSync(tempdir + "/movies.json")).toBe(true);
+  });
+
+  test("create movies.json, actors.json", async () => {
+    await db.createTable(["years", "actors"]);
+    expect(fs.existsSync(tempdir + "/years.json")).toBe(true);
+    expect(fs.existsSync(tempdir + "/actors.json")).toBe(true);
+    db.dropTable("years");
+    db.dropTable("actors");
+  });
+
+  test("check for records in new table", () => {
+    db.createTable("actors");
+    let data = db.get("actors");
+    expect(data.length).toBe(0);
+    db.dropTable("actors");
+  });
+
+  test("check insert", () => {
+    const rec = db.insert("movies", {
+      title: "Mission Impossible"
     });
+    expect(rec[0].title).toBe("Mission Impossible");
+  });
 
-    afterEach(() => {
-        db.truncateTable('movies');
+  test("insert arrayed data", () => {
+    const rec = db.insert("movies", [
+      { title: "Mission Impossible" },
+      { title: "Twilight" }
+    ]);
+
+    expect(rec.length).toBe(2);
+  });
+
+  test("get on db", () => {
+    db.insert("movies", {
+      title: "Mission Impossible"
     });
+    const recs = db.get("movies");
+    expect(recs.length).toBeGreaterThan(0);
+  });
 
-    afterAll(() => {
-        del.sync(tempdir + '/**', {
-            force: true
-        });
-        db = null;
-    })
+  test("get on non-existing table", () => {
+    let rec = db.get("test");
+    expect(rec).toBeUndefined();
+  });
 
-    test('create movies.json', () => {
-        expect(fs.existsSync(tempdir + '/movies.json')).toBe(true);
+  test("check getById", () => {
+    const rec = db.insert("movies", {
+      title: "Mission Impossible"
     });
+    const rec1 = db.getById("movies", rec[0].id);
+    expect(rec1).toMatchObject(rec[0]);
+  });
 
-    test('create movies.json, actors.json', () => {
-        db.createTable(['years', 'actors']);
-        expect(fs.existsSync(tempdir + '/years.json')).toBe(true);
-        expect(fs.existsSync(tempdir + '/actors.json')).toBe(true);
-        db.dropTable('years');
-        db.dropTable('actors');
+  test("getWhere", () => {
+    db.insert("movies", [
+      { title: "Mission Impossible" },
+      { title: "Twilight" }
+    ]);
+
+    let rec = db.getWhere("movies", { title: "Twilight" });
+    expect(rec.length).toBe(1);
+  });
+
+  test("check updateById", () => {
+    let rec = db.insert("movies", {
+      title: "Mission Impossible"
     });
-
-    test('check for records in new table', () => {
-        db.createTable('actors');
-        let data = db.get('actors');
-        expect(data.length).toBe(0);
-        db.dropTable('actors');
+    rec = db.updateById("movies", rec[0].id, {
+      title: "Twilight"
     });
+    expect(rec.title).toBe("Twilight");
+  });
 
-    test('check insert', () => {
-        const rec = db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        expect(rec[0].title).toBe('Mission Impossible');
+  test("check updateWhere", () => {
+    db.insert("movies", {
+      title: "Mission Impossible"
     });
+    let rec = db.updateWhere(
+      "movies",
+      {
+        title: "Mission Impossible"
+      },
+      {
+        title: "Twilight"
+      }
+    );
+    expect(rec[0].title).toBe("Twilight");
+  });
 
-    test('insert arrayed data', () => {
-        const rec = db.insert('movies', [
-            { title: 'Mission Impossible' },
-            { title: 'Twilight' }
-        ]);
-
-        expect(rec.length).toBe(2);
+  test("removeById", () => {
+    let rec = db.insert("movies", {
+      title: "Mission Impossible"
     });
+    db.removeById("movies", rec[0].id);
+    rec = db.getById("movies", rec[0].id);
+    expect(rec).toBeFalsy();
+  });
 
-    test('get on db', () => {
-        db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        const recs = db.get('movies');
-        expect(recs.length).toBeGreaterThan(0);
+  test("removeWhere", () => {
+    db.insert("movies", [
+      { title: "Mission Impossible", favorite: true },
+      { title: "Twilight", favorite: true }
+    ]);
+
+    db.removeWhere("movies", { favorite: true });
+    let rec = db.getWhere("movies", { favorite: true });
+    expect(rec.length).toBe(0);
+  });
+
+  test("truncateTable", () => {
+    let rec = db.insert("movies", {
+      title: "Mission Impossible"
     });
+    expect(rec.length).toBe(1);
+    db.truncateTable("movies");
+    rec = db.get("movies");
+    expect(rec.length).toBe(0);
+  });
 
-    test('get on non-existing table', () => {
-        let rec = db.get('test');
-        expect(rec).toBeUndefined();
-    })
-
-    test('check getById', () => {
-        const rec = db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        const rec1 = db.getById('movies', rec[0].id);
-        expect(rec1).toMatchObject(rec[0]);
-    });
-
-    test('getWhere', () => {
-        db.insert('movies', [
-            { title: 'Mission Impossible' },
-            { title: 'Twilight' }
-        ]);
-
-        let rec = db.getWhere('movies', { title: 'Twilight' });
-        expect(rec.length).toBe(1);
-    });
-
-    test('check updateById', () => {
-        let rec = db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        rec = db.updateById('movies', rec[0].id, {
-            title: 'Twilight'
-        });
-        expect(rec.title).toBe('Twilight');
-    });
-
-    test('check updateWhere', () => {
-        db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        let rec = db.updateWhere('movies', {
-            title: 'Mission Impossible'
-        }, {
-            title: 'Twilight'
-        });
-        expect(rec[0].title).toBe('Twilight');
-    });
-
-    test('removeById', () => {
-        let rec = db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        db.removeById('movies', rec[0].id);
-        rec = db.getById('movies', rec[0].id);
-        expect(rec).toBeFalsy();
-    });
-
-    test('removeWhere', ()=> {
-        db.insert('movies', [
-            { title: 'Mission Impossible', favorite: true },
-            { title: 'Twilight', favorite: true }
-        ]);
-
-        db.removeWhere('movies', { favorite: true });
-        let rec = db.getWhere('movies', {favorite: true});
-        expect(rec.length).toBe(0);
-    })
-
-    test('truncateTable', () => {
-        let rec = db.insert('movies', {
-            title: 'Mission Impossible'
-        });
-        expect(rec.length).toBe(1);
-        db.truncateTable('movies');
-        rec = db.get('movies');
-        expect(rec.length).toBe(0);
-    });
-
-    test('check dropTable', () => {
-        db.dropTable('movies');
-        expect(fs.existsSync(tempdir + '/movies.json')).toBe(false);
-    });
+  test("check dropTable", () => {
+    db.dropTable("movies");
+    expect(fs.existsSync(tempdir + "/movies.json")).toBe(false);
+  });
 });
--- jsondb.js
+++ jsondb.js
@@ -1,113 +1,119 @@
-const Utils = require('./lib/utils');
-const lodash = require('lodash');
-const lodashid = require('lodash-id');
+const Utils = require("./lib/utils");
+const lodash = require("lodash");
+const lodashid = require("lodash-id");
 const _ = Object.assign({}, lodash, lodashid);
 
 function jsondb(dataBasePath) {
-    let utils = new Utils(dataBasePath);
-    let cache = {};
+  let utils = new Utils(dataBasePath);
+  let cache = {};
 
-    utils.mkdirSync();
+  utils.mkdirSync();
 
-    function __insert(tableName, data) {
-        let rec = _.insert(cache[tableName], data);
-        return rec;
+  function __insert(tableName, data) {
+    let rec = _.insert(cache[tableName], data);
+    return rec;
+  }
+
+  async function __save(tableName) {
+    await utils.save(tableName, cache[tableName]);
+  }
+
+  // Clean cache
+  this.cleanCache = function() {
+    cache = {};
+  };
+
+  // Create table JSON
+  this.createTable = async function(tableName) {
+    let tableData;
+    if (typeof tableName === "string") {
+      tableData = await utils.createTable(tableName);
+      cache[tableName] = tableData;
+    } else if (tableName.map) {
+      for (const table of tableName) {
+        tableData = await utils.createTable(table);
+        cache[table] = tableData;
+      }
     }
+    return tableData;
+  };
 
-    // Clean cache
-    this.cleanCache = function () {
-        cache = {};
-    }
-
-    // Create table JSON
-    this.createTable = function (tableName) {
-        let tableData;
-        if (typeof tableName === 'string') {
-            tableData = utils.createTable(tableName);
-            cache[tableName] = tableData;
-        } else if (tableName.map) {
-            _.each(tableName, function (table) {
-                tableData = utils.createTable(table);
-                cache[table] = tableData;
-            });
-        }
-        return tableData;
-    }
-
-    // CURD Functions
-    this.get = function (tableName) {
-        try {
-            if (utils.checkTable(tableName)) {
-                if (!cache[tableName] || cache[tableName].length === 0) {
-                    const data = utils.readFile(tableName);
-                    cache[tableName] = data;
-                    return data;
-                } else {
-                    return cache[tableName];
-                }
-            } else {
-                return undefined;
-            }
-        } catch (error) {}
-    }
-
-    this.getById = function (tableName, id) {
-        return _.getById(cache[tableName], id);
-    }
-
-    this.getWhere = function (tableName, whereAttrs) {
-        const data = this.get(tableName);
-        return _.filter(data, whereAttrs);
-    }
-
-    this.insert = function (tableName, data) {
-        let rec = [];
-        if (data.map) {
-            _.each(data, function (item) {
-                let rec1 = __insert(tableName, item);
-                rec.push(rec1);
-            });
+  // CURD Functions
+  this.get = async function(tableName) {
+    try {
+      if (utils.checkTable(tableName)) {
+        if (!cache[tableName] || cache[tableName].length === 0) {
+          const data = await utils.readFile(tableName);
+          cache[tableName] = data;
+          return data;
         } else {
-            let rec1 = __insert(tableName, data);
-            rec.push(rec1);
+          return cache[tableName];
         }
-        utils.save(tableName, cache[tableName]);
-        return rec;
-    }
+      } else {
+        return undefined;
+      }
+    } catch (error) {}
+  };
 
-    this.updateById = function (tableName, id, data) {
-        const rec = _.updateById(cache[tableName], id, data);
-        utils.save(tableName, cache[tableName]);
-        return rec;
-    }
+  this.getById = async function(tableName, id) {
+    if (!cache[tableName] || cache[tableName].length === 0)
+      await this.get(tableName);
+    return _.getById(cache[tableName], id);
+  };
 
-    this.updateWhere = function (tableName, whereAttrs, attrs) {
-        const recs = _.updateWhere(cache[tableName], whereAttrs, attrs);
-        utils.save(tableName, cache[tableName]);
-        return recs;
-    }
+  this.getWhere = async function(tableName, whereAttrs) {
+    const data = await this.get(tableName);
+    return _.filter(data, whereAttrs);
+  };
 
-    this.removeById = function (tableName, id) {
-        _.removeById(cache[tableName], id);
-        utils.save(tableName, cache[tableName]);
+  this.insert = async function(tableName, data) {
+    let rec = [];
+    if (data.map) {
+      for (let item of data) {
+        let rec1 = __insert(tableName, item);
+        rec.push(rec1);
+      }
+    } else {
+      let rec1 = __insert(tableName, data);
+      rec.push(rec1);
     }
+    await __save(tableName);
+    return rec;
+  };
 
-    this.removeWhere = function (tableName, whereAttrs) {
-        _.removeWhere(cache[tableName], whereAttrs);
-        utils.save(tableName, cache[tableName]);
-    }
+  this.updateById = async function(tableName, id, data) {
+    const rec = _.updateById(cache[tableName], id, data);
+    await __save(tableName);
+    return rec;
+  };
 
-    this.truncateTable = function (tableName) {
-        cache[tableName] = [];
-        utils.save(tableName, []);
-    }
+  this.updateWhere = async function(tableName, whereAttrs, attrs) {
+    const recs = _.updateWhere(cache[tableName], whereAttrs, attrs);
+    await __save(tableName);
+    return recs;
+  };
 
-    this.dropTable = function (tableName) {
-        delete cache[tableName];
-        utils.dropTable(tableName);
-    }
+  this.removeById = async function(tableName, id) {
+    _.removeById(cache[tableName], id);
+    await __save(tableName);
+  };
 
-    return this;
+  this.removeWhere = async function(tableName, whereAttrs) {
+    _.removeWhere(cache[tableName], whereAttrs);
+    await __save(tableName);
+  };
+
+  this.truncateTable = async function(tableName) {
+    cache[tableName] = [];
+    utils.save(tableName, []);
+  };
+
+  this.dropTable = function(tableName) {
+    delete cache[tableName];
+    utils.dropTable(tableName);
+  };
+
+  return this;
 }
 
 module.exports = jsondb;
